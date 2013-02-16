99fcbb26a9104150c2aac03f52a60a300ed53265
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2013-02-16 18:03:08 -0300

    Copied code from starter-kit-misc.el

--- init.el
+++ init.el
@@ -23,3 +23,45 @@
 ;; 2012.07.17
 ;; bind M-j to delete-indentation (similar to 'J' in vim, to join lines)
 (global-set-key (kbd "M-j") 'delete-indentation)
+
+;; Copied from the Emacs Starter Kit
+;; github.com/technomancy/emacs-starter-kit/blob/v2/starter-kit-misc.el
+
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  (tooltip-mode -1)
+  (mouse-wheel-mode t)
+  (blink-cursor-mode -1))
+
+(setq visible-bell t
+      inhibit-startup-message t
+      color-theme-is-global t
+      sentence-end-double-space nil
+      shift-select-mode nil
+      mouse-yank-at-point t
+      uniquify-buffer-name-style 'forward
+      whitespace-style '(face trailing lines-tail tabs)
+      whitespace-line-column 80
+      ediff-window-setup-function 'ediff-setup-windows-plain
+      oddmuse-directory (concat user-emacs-directory "oddmuse")
+      save-place-file (concat user-emacs-directory "places")
+      backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+      diff-switches "-u")
+
+;; Highlight matching parentheses when the point is on them.
+(show-paren-mode 1)
+
+;; ido-mode is like magic pixie dust!
+(ido-mode t)
+;; (ido-ubiquitous t)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
